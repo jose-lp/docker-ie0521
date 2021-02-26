55138777f033c3ac2eb2b0c7d765c1dd8d27a043
José <joselp1695@gmail.com>
2021-02-26 16:28:34 -0600

    p2

--- p/mpgt/gprof.sh
+++ p/mpgt/gprof.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-MultipartiteGraphTracker -i /tmp/result.avi -o /salida.avi -c /tmp/config.yaml -t jam-crc-4k.yaml -I
+./MultipartiteGraphTracker -i /tmp/result.avi -o /tmp/salida.avi -c /tmp/config.yaml -t /tmp/tracks.yaml -I
 
-
+gprof MultipartiteGraphTracker gmon.out > profile-data.txt
